8ab5543f6fbd6a437a619b407b7c42e79a359afa
LucasLopes-Faculdade <68665689+lucaslopesx@users.noreply.github.com>
2021-08-25 21:05:05 -0300

    Update Form1.Designer.cs

--- Linguagem de Programação II/C#/Aula02/Projeto02/Form1.Designer.cs
+++ Linguagem de Programação II/C#/Aula02/Projeto02/Form1.Designer.cs
@@ -49,7 +49,7 @@ namespace Projeto02
             "Enfermagem"});
             this.listBox1.Location = new System.Drawing.Point(40, 156);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 94);
+            this.listBox1.Size = new System.Drawing.Size(142, 94);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
